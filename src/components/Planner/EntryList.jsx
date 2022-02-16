361e35d2c2f0fc7ef7388b988330f6193b5e65c7
mira-kine <mirak0419@gmail.com>
2022-02-16 15:33:43 -0800

    header count updates with number of entires

--- src/components/Planner/EntryList.jsx
+++ src/components/Planner/EntryList.jsx
@@ -3,16 +3,6 @@ import Entry from './EntryItem';
 import styles from './EntryList.css';
 
 export default function Entries() {
-  // TODO: Replace with the entries stored in PlannerContext
-  // const entries = [
-  //   {
-  //     id: 0,
-  //     title: 'Replace me!',
-  //     content: '`content` should only be visible on the detail view',
-  //     date: new Date()
-  //   },
-  // ];
-
   const { entries } = useEntries();
 
   return (
